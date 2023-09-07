4bed5e503044fbd20f71c061a03ea02b334e41a7
siddnaidu <siddnaidu@gmail.com>
2023-09-07 13:45:05 -0400

    Updating for PWA

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const { id, name, email } = props;
     return (
         <div className="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
-            <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
+            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
